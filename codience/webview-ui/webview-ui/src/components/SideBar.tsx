51df96330c3e1e41ca8fc2e1dcaa4d7c6907d93d
SamaAshrafAhmed <semsemsama2004@gmail.com>
2025-10-01 09:41:20 +0300

    added link to change current repo

--- codience/webview-ui/webview-ui/src/components/SideBar.tsx
+++ codience/webview-ui/webview-ui/src/components/SideBar.tsx
@@ -1,18 +1,19 @@
-import {  NavLink } from 'react-router-dom';
-import './styles/SideBar.css'
+import { NavLink } from "react-router-dom";
+import "./styles/SideBar.css";
 const SideBar = () => {
   return (
     <div className="sideBar">
       <h2 className="logo">Codience</h2>
-          <nav className="navBar">
-              <NavLink to='/home'>Home</NavLink>
-              <NavLink to='/dashboard'>Dashboard</NavLink>
-              <NavLink to='/reviewers'>Reviewers Analytics</NavLink>
-              <NavLink to='/profile'>Profile</NavLink>
+      <nav className="navBar">
+        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+        <NavLink to="/reviewers">Reviewers Analytics</NavLink>
+        <NavLink to="/profile">Profile</NavLink>
+        <NavLink to="/getRepo">Change Repo</NavLink>
       </nav>
       <button className="signOut">Sign Out</button>
     </div>
   );
-}
+};
 
-export default SideBar
+export default SideBar;
