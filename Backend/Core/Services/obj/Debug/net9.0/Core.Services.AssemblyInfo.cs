ee493676d9ce0cfa667be7822552c23a633e5b6f
MalakHisham <malak.hesham10011@gmail.com>
2026-05-10 22:32:29 +0300

    fixxing conflicts

--- Backend/Core/Services/obj/Debug/net9.0/Core.Services.AssemblyInfo.cs
+++ Backend/Core/Services/obj/Debug/net9.0/Core.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae9a23ded244112bc69f43c86cccb5062cc36a86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654415875a0b90deb617af5e64bda5958d149d2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
